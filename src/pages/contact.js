0da67184a48c16753c72fd3238dc020487d2363c
disharjayantha <dishuj15@gmail.com>
2021-01-11 21:53:34 +0530

    x axis margin changed for mobile

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -5,7 +5,7 @@ export default function Contact() {
   return (
     <div>
       <Layout />
-      <div className="mx-5 md:mx-10 px-4 md:px-10">
+      <div className="mx-1 md:mx-10 px-4 md:px-10">
         <h2 className="font-semibold text-5xl">Email:</h2>
         <p className="font-medium text-xl md:text-2xl pt-2">
           disharjayantha@gmail.com
