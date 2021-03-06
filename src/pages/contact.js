75cdabf9f44ac6a485018bdd4b36c80283acb786
disharjayantha <dishuj15@gmail.com>
2021-03-06 18:34:35 +0530

    changed contact email github links to clickable

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -8,14 +8,24 @@ export default function Contact() {
       <title>Contact</title>
       <div className="mx-1 md:mx-10 px-4 md:px-10">
         <h2 className="font-semibold text-5xl">Email:</h2>
-        <p className="font-medium text-xl md:text-2xl pt-2">
-          disharjayantha@gmail.com
+        <p className="font-medium text-xl md:text-2xl pt-2 hover:text-green-600">
+          <a
+            href="mailto:disharjayantha@gmail.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            disharjayantha@gmail.com
+          </a>
         </p>
-        <p className="font-medium text-xl md:text-2xl pt-2">
-          disharjayantha@protonmail.com
+        <p className="font-medium text-xl md:text-2xl pt-2 hover:text-purple-700">
+          <a href="mailto:disharjayantha@protonmail.com">
+            disharjayantha@protonmail.com
+          </a>
         </p>
-        <p className="font-medium text-xl md:text-2xl pt-2">
-          https://github.com/disharjayanth
+        <p className="font-medium text-xl md:text-2xl pt-2 hover:text-blue-700">
+          <a href="https://github.com/disharjayanth">
+            https://github.com/disharjayanth
+          </a>
         </p>
       </div>
     </div>
