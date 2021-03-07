5d610dbc20fcbd2be83309acd9ff166924b037d2
disharjayantha <dishuj15@gmail.com>
2021-03-07 14:10:09 +0530

    changed back to all blue on hover

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -8,7 +8,7 @@ export default function Contact() {
       <title>Contact</title>
       <div className="mx-1 md:mx-10 px-4 md:px-10">
         <h2 className="font-semibold text-5xl">Email:</h2>
-        <p className="font-medium text-xl md:text-2xl pt-2 hover:text-green-600">
+        <p className="font-medium text-xl md:text-2xl pt-2 hover:text-blue-700">
           <a
             href="mailto:disharjayantha@gmail.com"
             target="_blank"
@@ -17,8 +17,12 @@ export default function Contact() {
             disharjayantha@gmail.com
           </a>
         </p>
-        <p className="font-medium text-xl md:text-2xl pt-2 hover:text-purple-700">
-          <a href="mailto:disharjayantha@protonmail.com">
+        <p className="font-medium text-xl md:text-2xl pt-2 hover:text-blue-700">
+          <a
+            href="mailto:disharjayantha@protonmail.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             disharjayantha@protonmail.com
           </a>
         </p>
